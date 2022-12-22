ad697c65983c417a4a920b118cbe2e92bdc3a352
John Rooney <rooneyjohn@gmail.com>
2022-12-21 19:23:44 -0500

    Got delete almost working right.  If you add an item, and then hit delete, it doesn't work right.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const url = 'http://localhost:3000/ramens';
     let ramensArray;
     let myCurrentRamen;
+    let myCurrentImage;
 
     //Gather up the toops
     const ramenMenu = document.querySelector('#ramen-menu');
@@ -10,17 +11,35 @@ document.addEventListener('DOMContentLoaded', () => {
     const ramenMenuRest = document.querySelector('.restaurant');
     const ramenMenuRating = document.querySelector('#rating-display');
     const ramenMenuComment = document.querySelector('#comment-display');
+
     const newRamenForm = document.querySelector('#new-ramen');
     const newRamenButton = document.querySelector('form');
     newRamenButton.addEventListener('submit', (e) => {
         e.preventDefault()
         newRamenButtonClicked();
     })
+
     const newName = document.querySelector('#new-name');
     const newRest = document.querySelector('#new-restaurant');
     const newImage = document.querySelector('#new-image');
     const newRating = document.querySelector('#new-rating');
     const newComment = document.querySelector('#new-comment');
+
+    //Delete Button
+    const deleteButton = document.querySelector('h4');
+    deleteButton.innerHTML = "Delete Current Ramen";
+    deleteButton.addEventListener('click', (e) => {
+        //console.log(myCurrentRamen);
+        fetch(`${url}/${myCurrentRamen.id}`, {
+            method: 'DELETE',
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(myCurrentImage)
+            myCurrentImage.remove();
+            setFirstImage();
+        })
+    })
 
     function getAllData() {
         fetch(url)
@@ -37,15 +56,17 @@ document.addEventListener('DOMContentLoaded', () => {
         .then((response) => response.json())
         .then((data) => {
             imageClicked(data[0])
+            myCurrentRamen = data[0]
+            myCurrentImage = document.querySelector('img');
         })
     }
 
     function addRamenToNav(ramen) {
-        //create the image
         const img = document.createElement('img');
         img.src = ramen.image;
-        //Image will need to be a button
-        img.addEventListener('click', () => {
+        img.dataset.place = ramen.id;
+        img.addEventListener('click', (e) => {
+            myCurrentImage = e.target;
             imageClicked(ramen);
         })
         // This deals with the case where there is no image
@@ -53,7 +74,6 @@ document.addEventListener('DOMContentLoaded', () => {
             e.target.src = "https://curriculum-content.s3.amazonaws.com/phase-1/phase-1-mock-cc-ramen-rater/demo-gif.gif"
             e.onerror = null;
         })
-        //append the image to ramenMenu
         ramenMenu.appendChild(img);
     }
     function imageClicked(ramen) {
@@ -67,6 +87,7 @@ document.addEventListener('DOMContentLoaded', () => {
             e.target.src = "https://curriculum-content.s3.amazonaws.com/phase-1/phase-1-mock-cc-ramen-rater/demo-gif.gif"
             e.onerror = null;
         })
+        myCurrentRamen = ramen;
     }
 
     // Deal with the form meow
@@ -93,6 +114,7 @@ document.addEventListener('DOMContentLoaded', () => {
         })
         newRamenForm.reset();
     }
+
     getAllData();
     setFirstImage()
 });
